20a5380b05da92d35a8398eff4aaa6e0520228c6
leonardo44ls <leonardo44ls@517bda17-53e6-cab0-b21c-f43ced968a58>
2014-07-27 17:12:59 +0000

    Teste da classe TelaAcoes

--- PlantAcao/testes/view/TesteTelaAcoes.java
+++ PlantAcao/testes/view/TesteTelaAcoes.java
@@ -4,19 +4,27 @@ import static org.junit.Assert.*;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
-
-import javax.swing.ImageIcon;
 
 import org.junit.Test;
 
 public class TesteTelaAcoes {
 
+	TelaAcoes telaAcoes =new TelaAcoes(600, 480);
+	
+	@Test
+	public void testeConstrutor() {
+		
+		assertEquals("Largura da imagem da tela errada!", 600, telaAcoes.getTelaImagem().getWidth(null), 0);
+		assertEquals("Altura da imagem da tela errada!", 480, telaAcoes.getTelaImagem().getHeight(null), 0);
+		assertEquals("Não carregou o Array de botões", 1, telaAcoes.getBotoes().size(), 0);
+		
+		
+	}
+	
 	@Test
 	public void testeGetTelaImagem() {
-		TelaAcoes telaAcoes =new TelaAcoes(600, 480);
 		
 		assertEquals("Erro ao carregar imagem tela!", new Image() {
 			@Override
